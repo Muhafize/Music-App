dc9d8a2737732147697797811c64c3e894b6c4cf
Muhafize <muhafizahmede@gmail.com>
2025-01-10 19:46:52 +0300

    up

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001; // Change the port to 5001
 
 const httpServer = createServer(app);
 initializeSocket(httpServer);
