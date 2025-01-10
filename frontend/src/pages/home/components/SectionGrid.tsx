da962fbae5f213987f3632331841aa5e3b91e7ed
Muhafize <muhafizahmede@gmail.com>
2025-01-10 19:31:19 +0300

    best

--- frontend/src/pages/home/components/SectionGrid.tsx
+++ frontend/src/pages/home/components/SectionGrid.tsx
@@ -1,4 +1,4 @@
-import { Song } from "@/types";
+import { Song } from "@/types"
 import SectionGridSkeleton from "./SectionGridSkeleton";
 import { Button } from "@/components/ui/button";
 import PlayButton from "./PlayButton";
@@ -7,10 +7,9 @@ type SectionGridProps = {
     title: string;
     songs: Song[];
     isLoading: boolean;
-};
-
+}
 const SectionGrid = ({ songs, title, isLoading }: SectionGridProps) => {
-    if (isLoading) return <SectionGridSkeleton />;
+    if (isLoading) return <SectionGridSkeleton />
     return (
         <div className="mb-8">
             <div className="flex items-center justify-between mb-4">
@@ -22,11 +21,13 @@ const SectionGrid = ({ songs, title, isLoading }: SectionGridProps) => {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                 {songs.map((song) => (
-                    <div key={song._id} className="bg-zinc-800/40 p-4 rounded-md hover:bg-zinc-700/40 transition-all group cursor-pointer">
+                    <div key={song._id} className="bg-zinc-800/40 p-4 rounded-md hover:bg-zinc-700/40 transition-all group cursor-pointer"
+                    >
                         <div className="relative mb-4">
                             <div className="aspect-square rounded-md shadow-lg overflow-hidden">
                                 <img src={song.imageUrl} alt={song.title} className="w-full object-cover transition-transform duration-300 group-hover:scale-105" />
                             </div>
+                            {/* Todo: add play button */}
                             <PlayButton song={song} />
                         </div>
                         <h3 className="font-medium mb-2 truncate">{song.title}</h3>
@@ -35,7 +36,7 @@ const SectionGrid = ({ songs, title, isLoading }: SectionGridProps) => {
                 ))}
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default SectionGrid;
+export default SectionGrid
