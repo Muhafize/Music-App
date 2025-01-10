85eb0849ef557172011bf2c7f084d1e1ce7c8fba
Muhafize <muhafizahmede@gmail.com>
2025-01-10 20:48:42 +0300

    up

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
-const PORT = process.env.PORT || 5001; // Change the port to 5001
+const PORT = process.env.PORT || 5000;
 
 const httpServer = createServer(app);
 initializeSocket(httpServer);
@@ -67,6 +67,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+// Define a route handler for the root route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Spotify Clone API");
+});
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
